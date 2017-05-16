31b521d0d539006971dae52a4775cd5855804f56
berndhopp <bernd@vaadin.com>
2017-05-16 11:41:50 +0200

    javadoc for NeedsReflections, NeedsInjector
    (cherry picked from commit 656ca28)

--- src/main/java/com/vaadin/guice/annotation/Configuration.java
+++ src/main/java/com/vaadin/guice/annotation/Configuration.java
@@ -1,8 +1,11 @@
 package com.vaadin.guice.annotation;
 
 import com.google.inject.Module;
+import com.google.inject.Provider;
 
 import com.vaadin.guice.server.GuiceVaadinServlet;
+
+import org.reflections.Reflections;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -25,7 +28,19 @@ public @interface Configuration {
 
     /**
      * An array of classes for modules to be installed by guice. Each of these classes must have a
-     * default ( no-args ) constructor
+     * default ( no-args ) constructor. There are three interfaces that will be recognized when implemented
+     * by one of the classes in modules:
+     *
+     * Every class that implements {@link com.vaadin.guice.server.NeedsInjector} will have it's
+     * {@link com.vaadin.guice.server.NeedsInjector#setInjectorProvider(Provider)} method called with a provider
+     * for the {@link com.google.inject.Injector} that holds the Guice-context. The {@link Provider} will throw
+     * a {@link NullPointerException} on {@link Provider#get()} if the {@link com.google.inject.Injector} is not ready
+     * yet.
+     *
+     * Every class that implements {@link com.vaadin.guice.server.NeedsReflections} will have it's
+     * {@link com.vaadin.guice.server.NeedsReflections#setReflections(Reflections)} method called with the
+     * {@link Reflections} that are being used to set up the Guice-context.
+     *
      */
     Class<? extends Module>[] modules() default {};
 
