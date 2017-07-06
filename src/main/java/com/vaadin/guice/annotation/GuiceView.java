ea0d391ac9d5989c4c10b9039f145712815f4564
Bernd Hopp <bernd@vaadin.com>
2017-07-06 14:48:50 +0200

    get rid of GuiceView#USE_CONVENTIONS and rename GuiceView#name to GuiceView#value

--- src/main/java/com/vaadin/guice/annotation/GuiceView.java
+++ src/main/java/com/vaadin/guice/annotation/GuiceView.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  * the {@link com.vaadin.navigator.ViewProvider}.
  *
  * <pre>
- * &#064;GuiceView(name = &quot;&quot;)
+ * &#064;GuiceView(&quot;&quot;)
  * public class MyDefaultView extends CustomComponent implements View {
  *     // ...
  * }
@@ -28,19 +28,9 @@ import java.lang.annotation.Target;
 public @interface GuiceView {
 
     /**
-     * USE_CONVENTIONS is treated as a special case that will cause the automatic View mapping to
-     * occur.
-     */
-    String USE_CONVENTIONS = "USE CONVENTIONS";
-
-    /**
      * The name of the view. This is the name that is to be passed to the {@link
      * com.vaadin.navigator.Navigator} when navigating to the view. There can be multiple views with
      * the same name as long as they belong to separate UI subclasses.
-     *
-     * If the default value {@link #USE_CONVENTIONS} is used, the name of the view is derived from
-     * the class name so that e.g. UserDetailView becomes "user-detail". Although auto-generated
-     * view names are supported, using explicit naming of views is strongly recommended.
      */
-    String name() default USE_CONVENTIONS;
+    String value();
 }
