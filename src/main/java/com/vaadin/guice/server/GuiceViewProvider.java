ba1a088c1242044cd26dfe5f62104ef91565c9f3
berndhopp <bernd@vaadin.com>
2017-07-17 15:14:03 +0200

    simplification

--- src/main/java/com/vaadin/guice/server/GuiceViewProvider.java
+++ src/main/java/com/vaadin/guice/server/GuiceViewProvider.java
@@ -9,7 +9,6 @@ import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Strings.isNullOrEmpty;
 import static java.util.stream.Collectors.toMap;
 
 /**
